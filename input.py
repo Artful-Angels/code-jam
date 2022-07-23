74b2bc541d372d957f5e818001060c620bea1ceb
Alfonzitos <nylenalfons@gmail.com>
2022-07-23 20:13:35 +0200

    Reworked command handling
    Can now identify a command and args in a string containing non-relevant text so long the correct syntax is adhered by

--- input.py
+++ input.py
@@ -10,13 +10,16 @@ def message_handler(message: str) -> None:
     pass
 
 
-def command_handler(message: str, cmd: str) -> None:
+def command_handler(message: list, index: int) -> None:
     """
     Handles the commands
-
     For each command there will be a handler
     """
+
+    cmd = message[index]
+
     match cmd:
+        # prodivde args to command function through list slicing however many args expected after the command index
         case "del":
             print("Delete command activated")
         case _:  # If the command isn't in the list, skip
@@ -32,19 +35,18 @@ def command_validator(message: str) -> list:
     If the command is invalid, returns False and "Too many commands passed, maximum of 1 allowed"
     """
     command_count = 0   # Command count
-    command = ""        # Null handling
-
-    for i in message.lower().split():
-        if i in commands:  # only allows for 1 command per string
+    command_index = None 
+    for index, value in enumerate(message.lower().split()):
+        if value in commands:  # only allows for 1 command per string
 
             # If count is 1, that means it has more commands in it, so return False
             if command_count >= 1:
                 return [False, "Too many commands passed, maximum of 1 allowed"]
 
             command_count += 1
-            command = i
+            command_index = index
 
-    command_handler(message, command)  # In the end, pass the message and command to the message handler to execute it
+    command_handler(message.lower().split(), command_index)  # In the end, pass the message and command to the message handler to execute it
 
     return [True]  # Tell the runner that the message was a command
 
@@ -65,7 +67,8 @@ def input_handler(message: str) -> None:
             validator_response = command_validator(message)  # Validates if the message is a command
 
             if not validator_response[0]:
-                pass  # tell the user what the error is. Error message is ret[1]
+                pass  # tell the user what the error is. Error message is validator_response[1]
 
     if not is_command:
         message_handler(message)  # Passes the non-command message to the message handler
+
