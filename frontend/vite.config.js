a5ea00ac4f7a671072c03126ab90b32b3cdf886f
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-07-24 05:16:19 +0200

    Allow for hot reloads when running with Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
