c049fd78c3e98dd836899e2e127926119e979b21
karamfarhan <www.karam777krm@gmail.com>
2022-07-26 20:32:00 +0300

    Update/Fix chat consumer

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -17,16 +17,17 @@ class GameConsumer(AsyncWebsocketConsumer):
 
         )
         GameMembers = cache.get(f'game:members:{self.game_id}')
-        if len(GameMembers.keys()) >= 4:
-            
-            GameLogic = cache.get(f'game:logic:{self.game_id}')
-            Game = {
-                'game_members':GameMembers,
-                "game_logic":GameLogic
-            }
-            await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": Game})
-        else:
-            await self.channel_layer.group_send(self.game_id, {"type": "Send_Memebers", "data": GameMembers})
+        if GameMembers:
+            if len(GameMembers.keys()) >= 4:
+                
+                GameLogic = cache.get(f'game:logic:{self.game_id}')
+                Game = {
+                    'game_members':GameMembers,
+                    "game_logic":GameLogic
+                }
+                await self.channel_layer.group_send(self.game_id, {"type": "Send_Game", "data": Game})
+            else:
+                await self.channel_layer.group_send(self.game_id, {"type": "Send_Memebers", "data": GameMembers})
 
         await self.accept()
 
@@ -83,28 +84,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope['url_route']['kwargs']['username']
         self.game_code = self.scope['url_route']['kwargs']['game_code']
-        self.room_group_name = f"chat:{self.game_code}"
+        self.room_group_name = f"chat-{self.game_code}"
         print(self.room_group_name)
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        # await self.channel_layer.group_send( 
-        #     self.room_group_name,
-        #     {
-        #         'type':'send_notification',
-        #         'msg_type':'notify',
-        #         'username':self.user.username,
-        #         'message': f'{self.user.username} joined the chat',
-        #         'date': datetime.datetime.now().strftime("%I:%M %p | %d %b %Y")
-        #     }
-        # )
         await self.accept()
 
-
-    
-
-    
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -114,15 +101,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        print(text_data_json)
         message = text_data_json['message']
         username = text_data_json['username']
-        date_now = datetime.datetime.now().strftime("%I:%M %p | %d %b %Y")
-        await self.save_message(text_data_json)
+        date_now = datetime.datetime.now().strftime("%I:%M %p")
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'send_messages',
-                'msg_type':'con',
                 'message': message,
                 'username': username,
                 'date': date_now,
@@ -130,24 +116,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
     async def send_messages(self,event):
         message = event['message']
-        msg_type = event['msg_type']
         username = event['username']
         date = event['date']
         await self.send(text_data=json.dumps({
-            'msg_type':msg_type,
+            'method':'MSG',
             'message': message,
             'username': username,
             'date': date,
         }))
 
-    # async def send_notification(self,event):
-    #     message = event['message']
-    #     msg_type = event['msg_type']
-    #     username = event['username']
-    #     date = event['date']
-    #     await self.send(text_data=json.dumps({
-    #         'msg_type': msg_type,
-    #         'username': username,
-    #         'message': message,
-    #         'date': date,
-    #     }))
+    async def send_notification(self,event):
+        message = event['message']
+        date = event['date']
+        await self.send(text_data=json.dumps({
+            'method':'NTF',
+            'message': message,
+            'date': date,
+        }))
--- backend/game/routing.py
+++ backend/game/routing.py
@@ -4,5 +4,5 @@ from .consumers import GameConsumer,ChatConsumer
 
 ws_urlpatterns = [
     re_path(r'ws/game/(?P<game_id>\w+)/$',GameConsumer.as_asgi()),
-    re_path(r'ws/chat/(?P<game_code>\w+)/(?P<username>\w+)$',ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<game_code>\w+)/(?P<username>\w+)/$',ChatConsumer.as_asgi()),
 ]
