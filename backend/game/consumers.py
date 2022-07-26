5aed59d3b58d635bbfa1c6bc6e7ffd148abab5fe
karamfarhan <www.karam777krm@gmail.com>
2022-07-26 18:55:40 +0300

    Add chat consumer
    - chat consumer will handle the messages
    - receiving messages and send them to users

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,6 +1,11 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from django.core.cache import cache
+import datetime
+
+
+
+# GAME HANDLER
 class GameConsumer(AsyncWebsocketConsumer):
     ## conect
     async def connect(self):
@@ -50,8 +55,6 @@ class GameConsumer(AsyncWebsocketConsumer):
                 }
             )
 
-
-
     ## handlers
 
     ## click method handler
@@ -73,3 +76,78 @@ class GameConsumer(AsyncWebsocketConsumer):
             'data': data
         }))
     
+
+# MESSAGE HANDLER
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['url_route']['kwargs']['username']
+        self.game_code = self.scope['url_route']['kwargs']['game_code']
+        self.room_group_name = f"chat:{self.game_code}"
+        print(self.room_group_name)
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        # await self.channel_layer.group_send( 
+        #     self.room_group_name,
+        #     {
+        #         'type':'send_notification',
+        #         'msg_type':'notify',
+        #         'username':self.user.username,
+        #         'message': f'{self.user.username} joined the chat',
+        #         'date': datetime.datetime.now().strftime("%I:%M %p | %d %b %Y")
+        #     }
+        # )
+        await self.accept()
+
+
+    
+
+    
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+ 
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+        date_now = datetime.datetime.now().strftime("%I:%M %p | %d %b %Y")
+        await self.save_message(text_data_json)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_messages',
+                'msg_type':'con',
+                'message': message,
+                'username': username,
+                'date': date_now,
+            }
+        )
+    async def send_messages(self,event):
+        message = event['message']
+        msg_type = event['msg_type']
+        username = event['username']
+        date = event['date']
+        await self.send(text_data=json.dumps({
+            'msg_type':msg_type,
+            'message': message,
+            'username': username,
+            'date': date,
+        }))
+
+    # async def send_notification(self,event):
+    #     message = event['message']
+    #     msg_type = event['msg_type']
+    #     username = event['username']
+    #     date = event['date']
+    #     await self.send(text_data=json.dumps({
+    #         'msg_type': msg_type,
+    #         'username': username,
+    #         'message': message,
+    #         'date': date,
+    #     }))
