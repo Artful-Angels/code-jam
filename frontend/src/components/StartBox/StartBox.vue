7058ae8310141a16919f0f256711ce087441dd9f
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-07-27 00:02:25 +0200

    Add logic to handle game creation

--- frontend/src/components/StartBox/StartBox.vue
+++ frontend/src/components/StartBox/StartBox.vue
@@ -7,38 +7,46 @@
         <logo-v1 class="hidden sm:block" />
         <logo-v2 class="sm:hidden" />
       </div>
-      <form class="mt-8 space-y-6" @submit.prevent="submitGame">
-        <div class="rounded-md shadow-sm -space-y-px">
-          <label for="nickname" class="sr-only">Nickname</label>
-          <input
-            id="nickname"
-            name="nickname"
-            type="text"
-            autocomplete="nickname"
-            placeholder="Nickname"
-            v-model="nickname"
-            required
-          />
-          <label for="game-code" class="sr-only">Game code</label>
-          <input
-            id="game-code"
-            ref="game-code"
-            name="game_code"
-            type="number"
-            autocomplete="off"
-            placeholder="Game code"
-            class="pr-28 inline"
-            v-model="gameCode"
-            required
-          />
-          <a
-            href="#"
-            @click="updateGameCode(this.$refs['game-code'])"
-            style="margin-left: -94px"
-            class="font-mono relative z-50 text-blue-600 hover:text-blue-700 dark:hover:text-blue-500"
-            >NEW GAME</a
-          >
+      <form
+        class="mt-8 space-y-6"
+        @submit.prevent="submitGame(nickname, gameCode)"
+      >
+        <div>
+          <div class="rounded-md shadow-sm -space-y-px">
+            <label for="nickname" class="sr-only">Nickname</label>
+            <input
+              id="nickname"
+              name="nickname"
+              type="text"
+              autocomplete="nickname"
+              placeholder="Nickname"
+              v-model="nickname"
+              required
+            />
+            <label for="game-code" class="sr-only">Game code</label>
+            <input
+              id="game-code"
+              ref="game-code"
+              name="game_code"
+              type="number"
+              autocomplete="off"
+              placeholder="Game code"
+              class="pr-28 inline"
+              v-model="gameCode"
+              required
+            />
+            <a
+              href="#"
+              @click="updateGameCode(this.$refs['game-code'])"
+              style="margin-left: -94px"
+              class="font-mono relative z-50 text-blue-600 hover:text-blue-700 dark:hover:text-blue-500"
+              >NEW GAME</a
+            >
+          </div>
         </div>
+        <p id="error-box" class="text-red-500 ml-1 mt-1" hidden>
+          Something went wrong
+        </p>
         <div>
           <button
             class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 dark:bg-blue-600 hover:bg-blue-700 dark:hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -55,14 +63,15 @@
 import LogoV1 from "@/components/logos/LogoV1.vue";
 import LogoV2 from "@/components/logos/LogoV2.vue";
 import axios from "axios";
+import router from "@/router";
 
-let nickname = undefined
-let gameCode = undefined
+let nickname = undefined;
+let gameCode = undefined;
 
 function updateGameCode(element) {
-  const event = new Event('input')
-  element.value = generateGameCode()
-  element.dispatchEvent(event)
+  const event = new Event("input");
+  element.value = generateGameCode();
+  element.dispatchEvent(event);
 }
 
 function generateGameCode() {
@@ -70,14 +79,37 @@ function generateGameCode() {
   // TODO: Ensure that the game code never collides with already existing games
 }
 
-function submitGame() {
-  axios.post("http://localhost:8000/game/createorjoin/", {
-    "nickname": nickname,
-    "game_code": gameCode,
-  }).then((response) => {
-    console.log("Then", response)
-  }).catch((response) => {
-    console.log("Catch", response)
-  })
+function submitGame(nickname, gameCode) {
+  axios
+    .post("http://localhost:8000/game/createorjoin/", {
+      nickname: nickname,
+      game_code: gameCode,
+    })
+    .then((response) => {
+      console.log(response);
+      switch (response.status) {
+        case 200:
+          router.push({
+            path: "/game",
+            query: { code: gameCode.toString() },
+          });
+          break;
+        default:
+          showError("Unrecognized response!");
+      }
+    })
+    .catch((response) => {
+      if (response.response.status === 409) {
+        showError(
+          "A player has already joined with that name. Please choose a different one."
+        );
+      }
+    });
+}
+
+function showError(text) {
+  const error_box = document.getElementById("error-box");
+  error_box.innerText = text;
+  error_box.removeAttribute("hidden");
 }
 </script>
