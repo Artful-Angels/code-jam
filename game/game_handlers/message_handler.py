4a041c3d4115a504949edf82ca7d315ac10e5432
karamfarhan <www.karam777krm@gmail.com>
2022-07-30 10:11:23 +0300

    Integrate command fucntions with messages handler
    - delete_square with delte command
    - roll_winner with winner command

--- game/game_handlers/message_handler.py
+++ game/game_handlers/message_handler.py
@@ -1,4 +1,8 @@
+from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
+from django.core.cache import cache
+
+from .game_logic import delete_square, roll_winner
 
 channel_layer = get_channel_layer()
 
@@ -12,14 +16,30 @@ def command_handler(data: dict) -> None:
     For each command there will be a handler
     """
     cmd = data["cmd"]
+    nickname = data["username"]
+    game_code = data["game_code"]
+    game_state = cache.get(f"game:{game_code}")
     if cmd == "delete":
         # here the (delete) command logic will be
         # in any error case return False
-        print("changing the game status based on the delete command")
+        # try:
+        new_game_state = delete_square(game_state, nickname)
+        cache.set(f"game:{game_code}", new_game_state)
+        async_to_sync(channel_layer.group_send)(game_code, {"type": "Update_Game", "data": game_state})
+        # except:
+        #     return False
+
+        # print("changing the game status based on the delete command")
     elif cmd == "winner":
         # here the (winner) command logic will be
         # in any error case return False
-        print("changing the game status based on the winner command")
+        # try:
+        game_state = roll_winner(game_state, nickname)
+        cache.set(f"game:{game_code}", new_game_state)
+        async_to_sync(channel_layer.group_send)(game_code, {"type": "Update_Game", "data": game_state})
+        # except:
+        #     return False
+        # print("changing the game status based on the winner command")
 
     return True
 
@@ -30,9 +50,7 @@ def command_validator(message: str) -> list:
 
     If the command is invalid, returns False and "Too many commands passed, maximum of 1 allowed"
     """
-    message = (
-        message.lower().split()
-    )  # Splits the message into a lowercase list for further processing
+    message = message.lower().split()  # Splits the message into a lowercase list for further processing
     command_count = 0  # Command count
     command_index = None
 
@@ -64,9 +82,7 @@ def input_handler(data: dict) -> None:
     for i in message.lower().split():
         if i in commands:
             is_command = True
-            validator_response = command_validator(
-                message
-            )  # Validates if the message is a command
+            validator_response = command_validator(message)  # Validates if the message is a command
 
             if not validator_response[0]:
                 return [True]
