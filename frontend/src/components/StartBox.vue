c6b14c081944e618bab62943bc8cb474ec61348e
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-08-01 06:35:27 +0200

    Fix URL to use https in frontend

--- frontend/src/components/StartBox.vue
+++ frontend/src/components/StartBox.vue
@@ -89,7 +89,7 @@ function generateGameCode() {
 
 function submitGame(nickname, gameCode) {
   axios
-    .post(`http://${import.meta.env.VITE_API_BASE_URL}/game/createorjoin/`, {
+    .post(`https://${import.meta.env.VITE_API_BASE_URL}/game/createorjoin/`, {
       nickname: cleanupNickname(nickname),
       game_code: gameCode,
     })
