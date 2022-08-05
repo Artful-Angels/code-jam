885c6ff61ea4946021ade49af5c1a61ef121fad7
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-08-05 17:34:56 +0200

    Make sure SafeFlag is seen on Safari

--- frontend/src/components/icons/FlagSafe.vue
+++ frontend/src/components/icons/FlagSafe.vue
@@ -2,12 +2,13 @@
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
+    width="100%"
+    height="100%"
     fill="none"
     stroke="#52B788"
     stroke-width="3"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="feather feather-loader"
   >
     <line x1="12" y1="2" x2="12" y2="6"></line>
     <line x1="12" y1="18" x2="12" y2="22"></line>
