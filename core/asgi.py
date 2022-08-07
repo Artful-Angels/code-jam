691831fcc0208903fc62be24e46c7013bdb9df59
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-08-07 17:19:29 +0200

    Update backend config to match deploy
    Code changes here were primarily authored by @karamfarhan, but the Git
    history was lost when migrating from the `deploy-backend` branch to the
    dev branch

--- core/asgi.py
+++ core/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from game.routing import ws_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+os.environ.get("DJANGO_SETTINGS_MODULE")
 
 application = ProtocolTypeRouter(
     {
--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,11 +1,33 @@
+import os
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ["bugsweeper.herokuapp.com"]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
+    "https://bugsweeper.godi.se",
 ]
+
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
+        },
+    },
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
+        "TIMEOUT": 3600,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "MAX_ENTRIES": 2000,
+        },
+    }
+}
