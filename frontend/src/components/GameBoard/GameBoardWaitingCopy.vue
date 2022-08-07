eef25c06f04e1f6bb7631010340eeced25db43d8
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-08-07 20:49:41 +0200

    Fix word break in copy text

--- frontend/src/components/GameBoard/GameBoardWaitingCopy.vue
+++ frontend/src/components/GameBoard/GameBoardWaitingCopy.vue
@@ -1,11 +1,11 @@
 <template>
   <div
     data-tooltip-target="tooltip-default"
-    class="max-w-md hover:cursor-pointer py-3 px-5 rounded-md bg-gray-50 dark:bg-slate-800 dark:border-slate-700"
+    class="max-w-xl hover:cursor-pointer py-3 px-5 rounded-md bg-gray-50 dark:bg-slate-800 dark:border-slate-700"
     @click="copyToClipboard(copyText)"
   >
     <p
-      class="text-4xl font-bold text-gray-800 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-200 uppercase"
+      class="text-4xl font-bold text-gray-800 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-200 uppercase break-words"
     >
       {{ copyText }}
     </p>
